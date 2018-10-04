4c38e30b7269fc2e4bcaf728cf8cf4ded0545769
kunasireesha <kunasireesha5@gmail.com>
2018-10-04 12:10:26 +0530

    products

--- src/app/components/rec-products/rec-products.component.ts
+++ src/app/components/rec-products/rec-products.component.ts
@@ -24,10 +24,11 @@ export class RecProductsComponent implements OnInit {
   title;
   url;
   percentage;
-  showInput = false;
+  showInput = true;
   items = {
-    quantity: 0
+    quantity: 1
   }
+  selected;
   ngOnInit() {
     this.url = AppSettings.imageUrl;
     if (localStorage.userName !== undefined || localStorage.userData !== undefined) {
@@ -85,37 +86,53 @@ export class RecProductsComponent implements OnInit {
   }
 
   //add to cart
-  itemIncrease(id, skuId) {
-    let thisObj = this;
-    this.showInput = true;
-    thisObj.items.quantity = Math.floor(thisObj.items.quantity + 1);
-    this.getCart(thisObj.items.quantity, id, skuId);
-  }
-  itemDecrease(id, skuId) {
-    let thisObj = this;
-    if (thisObj.items.quantity === 1) {
-      return;
-    }
-    thisObj.items.quantity = Math.floor(thisObj.items.quantity - 1);
-    this.getCart(thisObj.items.quantity, id, skuId);
-  }
+  quantity;
   getCart(quantity, id, skuId) {
+    if (quantity === 0) {
+      this.quantity = 1;
+    } else {
+      this.quantity = quantity
+    }
     var inData = {
       _id: this.id,
       _session: localStorage.session,
       id_product: id,
       id_sku: skuId,
       op: "modify",
-      quantity: JSON.stringify(quantity),
+      quantity: JSON.stringify(this.quantity),
       wh_pincode: "560078",
       parent_warehouseid: JSON.parse(localStorage.parent_warehouseid),
       id_warehouse: JSON.parse(localStorage.id_warehouse)
     }
     this.loginService.getCart(inData).subscribe(response => {
       swal('Item added to cart', '', 'success');
+      this.items.quantity = this.quantity;
     }, err => {
       swal(err.json().message, '', 'error');
     })
+  }
+
+  itemIncrease(data, name, id, skuId, index) {
+    this.selected = index;
+    let thisObj = this;
+    if (localStorage.name !== name) {
+      thisObj.items.quantity = 0;
+    }
+    if (name === data.name) {
+      thisObj.showInput = true;
+      thisObj.items.quantity = Math.floor(thisObj.items.quantity + 1);
+      thisObj.getCart(thisObj.items.quantity, id, skuId);
+      localStorage.setItem('name', name);
+    }
+  }
+  itemDecrease(id, skuId, index) {
+    this.selected = index;
+    let thisObj = this;
+    if (thisObj.items.quantity === 1) {
+      return;
+    }
+    thisObj.items.quantity = Math.floor(thisObj.items.quantity - 1);
+    this.getCart(thisObj.items.quantity, id, skuId);
   }
 
 
