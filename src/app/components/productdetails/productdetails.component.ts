32652ce152f13bac3287761c632e0bef567bea09
kunasireesha <kunasireesha5@gmail.com>
2018-10-05 11:26:45 +0530

    new changes

--- src/app/components/productdetails/productdetails.component.ts
+++ src/app/components/productdetails/productdetails.component.ts
@@ -1,17 +1,21 @@
 import { DataService } from './../../services/login/login';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
-
+import { AppSettings } from '../../config';
 
 @Component({
   selector: 'app-productdetails',
   templateUrl: './productdetails.component.html',
-  styleUrls: ['./productdetails.component.less']
+  styleUrls: ['./productdetails.component.less','../product/product.component.less']
 })
 export class ProductdetailsComponent implements OnInit {
   id;
   prodId;
   product;
+  url;
+  firstPic;
+  specificProd;
+  percentage;
   constructor(private route: ActivatedRoute, public router: Router, public loginService: DataService) {
     this.route.queryParams.subscribe(params => {
       this.prodId = params.proId;
@@ -24,6 +28,7 @@ export class ProductdetailsComponent implements OnInit {
    }
 
   ngOnInit() {
+    this.url = AppSettings.imageUrl;
     this.productDetails();
     
   }
@@ -71,7 +76,21 @@ export class ProductdetailsComponent implements OnInit {
         lang:"en",
     }
     this.loginService.productDetails(inData).subscribe(response => {
-    this.product = response.json().product.pic[0];
+    this.product = response.json().product;
+    for(var i = 0; i < this.product.length; i++){
+      this.firstPic = this.url + this.product[i].pic[0].pic;
+    }
+    this.specificProd = response.json().specific_product[0].product;
+    for(var i = 0; i < this.specificProd.length; i++){
+      this.specificProd[i].image = this.url + this.specificProd[i].pic[0].pic;
+    }
+    for(var i = 0; i<this.specificProd.length;i++){
+      if (this.specificProd[i].sku[0].mrp !== undefined) {
+        this.percentage = this.specificProd[i].sku[0].selling_price / this.specificProd[i].sku[0].mrp * 100
+        this.specificProd[i].sku[0].percentage = this.percentage;
+      }
+    }
+    
     }, err => {
       console.log(err);
     })
